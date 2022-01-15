af901f0b8761b164d73ef27e6974b23890871ecd
arnamen <aew11111d@gmail.com>
2022-01-15 17:27:57 +0200

    added timer

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,23 @@
-import './style.scss';
+// basic setup
+import './base';
+//
+import { interval, mapTo, scan, takeWhile } from 'rxjs';
 
-window.document.title = process.env.PROJECT_TITLE;
+let initialValue = 15000; // ms
+const tick = 10; // ms
+
+function setValue(value: number | string) {
+    document.querySelector('#timer').innerHTML = value.toString();
+}
+
+setValue(initialValue);
+
+interval(tick).pipe(
+    mapTo(tick),
+    scan((initialValue, tick) => initialValue - tick, initialValue),
+    takeWhile(value => value >= 0)
+).subscribe({
+    next: setValue,
+    complete: () => setValue("Done!")
+    
+})
